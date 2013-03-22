235170ac1fec8301d09114f214f43d16f8d71319
Koty <koty@staplegun.us>
2013-03-22 09:57:52 -0500

    included a placeholder support helper in script.js. moved scss utilities from screen.scss to _utilities.scss. added a .gitignore for use on remote boxes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
-/* Author:
+/* Fake placeholder support in browsers that do not support the HTML input placeholder attribute. Ideally this would be paired with a javascript function that prevents form submit if the a given input's value is equivalent to the placeholder attribute. */
+/*
+function initPlaceholderSupport() {
+    if( ! Modernizr.input.placeholder ) {
+        $('[placeholder]').focus(function() {
+          var input = $(this);
+          if (input.val() == input.attr('placeholder')) {
+            input.val('');
+            input.removeClass('placeholder');
+          }
+        }).blur(function() {
+          var input = $(this);
+          if (input.val() == '' || input.val() == input.attr('placeholder')) {
+            input.addClass('placeholder');
+            input.val(input.attr('placeholder'));
+          }
+        }).blur();
+    }
+}
 
+initPlaceholderSupport();
 */
-
-
-
-
-
