382acf35408984d26c5e4ebb264764921f488df9
Darren Jaworski <darrenjaworski@gmail.com>
2014-03-05 11:06:55 -0600

    Streamlined js compression and suzy require.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,58 +1,62 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      dist: {
-        files: {
-          'assets/js/scripts.min.js': [
-          'assets/js/lib/*.js',
-          'assets/js/_*.js'
-		  ]
-		}
-	  }		  
-	},
-    sass: {
-	  dist: {
-	    options: {
-	      style: 'compressed',
-	      compass: 'true',
-	      sourcemap: false
-	    },
-	    files: {
-		    'assets/css/screen.css' : [
-		    'assets/sass/screen.scss'
-		   ]
-	    }
-	  }
-	},
-	watch: {
-	  options: {
-		livereload: true
-      },
-	  sass: {
-		files: [
-		  'assets/sass/*.scss',
-		  'assets/sass/partials/*.scss'
-		],
-		tasks: [
-		  'sass'
-		], 
-	  }
-    },
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            dist: {
+                files: {
+                    'assets/js/scripts.min.js': [
+                        'assets/js/lib/*.js',
+                        'assets/js/_*.js'
+                    ],
+                    'assets/js/ie.min.js': [
+                        'assets/js/ie/*.js'
+                    ]
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed',
+                    compass: true,
+                    sourcemap: false,
+                    lineNumbers: true,
+                    require: 'susy'
+                },
+                files: {
+                    'assets/css/screen.css': [
+                        'assets/sass/screen.scss'
+                    ]
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            sass: {
+                files: [
+                    'assets/sass/*.scss',
+                    'assets/sass/partials/*.scss'
+                ],
+                tasks: [
+                    'sass'
+                ],
+            }
+        },
+    });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task(s).
-  grunt.registerTask('default', [
-  	'uglify',
-  	'sass',
-  	'watch'
-  	]);
+    // Default task(s).
+    grunt.registerTask('default', [
+        'uglify',
+        'sass',
+        'watch'
+    ]);
 
 };
