bf6ba740db404b7a0d8fb210b116bd4eb7a6960e
aaron <alkrauss48@gmail.com>
2014-06-09 23:20:14 -0500

    Add skills page

---
+++ wp-content/themes/the_societea/skills.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Template Name: Skills
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+      <div class="subpage-header-image" style="background-image: url('<?php echo $cfs->get('header_image'); ?>');"
+  data-start="background-position: 50% 55%;"
+  data-400="background-position: 50% 45%;"
+></div>
+      <div class="subpage-header-overlay"></div>
+      <div class="subpage-title">
+        <div class="container">
+          <div class="inner-title-wrapper">
+            <div class="container-padding">
+              <h1><?php the_title(); ?></h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="plain" id="main-content">
+        <div class="container-padding">
+          <div class="container">
+            <div class="entry-content">
+              <?php the_post();the_content(); ?>
+            </div>
+            <?php $categories = $cfs->get('categories'); ?>
+            <?php if($categories): ?>
+              <?php foreach($categories as $category): ?>
+                <div class="skill-category">
+                  <h2 style="border-bottom: 3px solid <?php echo $category['color']; ?>"><?php echo $category['category_title']; ?></h2>
+                  <ul class="category-skills-list">
+                    <?php $skills = $category['skills']; ?>
+                    <?php if($skills): ?>
+                      <?php foreach($skills as $skill): ?>
+                        <li>
+                          <p><?php echo $skill['skill']; ?></p>
+                          <div class="skill-bar-wrapper">
+                            <div class="skill-bar <?php echo strtolower($category['category_title']); ?>-bar" style="width: <?php echo $skill['percentage']; ?>%;"
+                              data-bottom-top="width: 0%;"
+                              data--50-bottom-bottom="width: <?php echo $skill['percentage']; ?>%;"
+                            ></div>
+                          </div>
+                        </li>
+                      <?php endforeach; ?>
+                    <?php endif; ?>
+                  </ul>
+                </div>
+              <?php endforeach; ?>
+            <?php endif; ?>
+          </div>
+        </div>
+      </div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
