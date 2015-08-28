3daf92cd56c2f0641fddcb6b16ed186c130aa428
aaron <alkrauss48@gmail.com>
2015-08-28 15:17:03 -0500

    Remove og:url

--- wp-content/themes/the_societea/header.php
+++ wp-content/themes/the_societea/header.php
@@ -22,7 +22,6 @@
     <meta name="description" content="<?php the_post();echo get_the_excerpt(); ?>">
     <meta property="og:description" content="<?php echo get_the_excerpt(); ?>">
     <meta property="og:image" content="https://thesocietea.org/assets/images/dist/aaron-krauss.jpg">
-    <meta property="og:url" content="https://thesocietea.org">
     <meta property="og:title" content="<?php wp_title( '|', true, 'right' ); ?>">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimal-ui">
 
