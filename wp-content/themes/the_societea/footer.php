7dfbbf404264e46a1fb0f6a1b7ee6da354a649a9
aaron <alkrauss48@gmail.com>
2016-03-21 11:21:28 -0500

    Add autotrack to analytics

--- wp-content/themes/the_societea/footer.php
+++ wp-content/themes/the_societea/footer.php
@@ -33,13 +33,21 @@
       <script src="/assets/js/ie/ie.js"></script>
     <![endif]-->
     <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <!-- <script> -->
+    <!--   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ -->
+    <!--   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), -->
+    <!--   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) -->
+    <!--   })(window,document,'script','//www.google&#45;analytics.com/analytics.js','ga'); -->
+    <!--   ga('create', 'UA&#45;38589428&#45;1', 'thesocietea.org'); -->
+    <!--   ga('send', 'pageview'); -->
+    <!-- </script> -->
     <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
       ga('create', 'UA-38589428-1', 'thesocietea.org');
+      ga('require', 'autotrack');
       ga('send', 'pageview');
     </script>
+    <script async src='https://www.google-analytics.com/analytics.js'></script>
+    <script async src='/assets/js/autotrack.js'></script>
   </body>
 </html>
