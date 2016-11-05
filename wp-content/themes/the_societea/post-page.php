6e738017a12854059bdf30568acc0adc6cd24028
aaron <alkrauss48@gmail.com>
2016-11-04 21:01:25 -0500

    Add aria labels to the blog items

--- wp-content/themes/the_societea/post-page.php
+++ wp-content/themes/the_societea/post-page.php
@@ -6,9 +6,9 @@
  */
 ?>
 
-<a id="post-<?php the_ID(); ?>" <?php post_class(array('blog-item')); ?> href="<?php the_permalink( get_the_ID() ) ?>">
+<a aria-labelledby="post-title-<?php the_ID() ?>'" id="post-<?php the_ID(); ?>" <?php post_class(array('blog-item')); ?> href="<?php the_permalink( get_the_ID() ) ?>">
   <div class="blog-item__overlay">
-    <?php the_title( '<h2 class="blog-item__title">', '</h2>' ); ?>
+    <?php the_title( '<h2 class="blog-item__title" id="post-title-'.get_the_ID().'">', '</h2>' ); ?>
     <div class="blog-item__excerpt">
       <?php echo wp_trim_words(get_the_excerpt(), 12, '...'); ?>
     </div>
