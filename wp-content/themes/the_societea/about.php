ce4cf777a8c2a39b7403270476e644403e94b8de
aaron <alkrauss48@gmail.com>
2020-08-21 23:36:03 -0500

    Update about page

--- wp-content/themes/the_societea/about.php
+++ wp-content/themes/the_societea/about.php
@@ -30,9 +30,10 @@ get_header(); ?>
     data-700="left: 0px; opacity: 1;"
     data-850="left: 75px; opacity: 0;"
 >
-          My name is Aaron Krauss. I’m a full stack developer based out of Edmond, Oklahoma (near Oklahoma City).
+          My name is Aaron Krauss. I’m a full stack developer based out of OKC, Oklahoma.
           I was born in Germany in 1990, grew up in a German culture for 6 years, and still speak moderately fluent German.
           I currently work at Clevyr as a senior software engineer, and engage in miscellaneous development on my own time.
+          I'm a fan of Ruby, Python, JavaScript, C#, SQL - and honestly, just about anything.
         </p>
     </div>
   </div>
@@ -73,12 +74,12 @@ get_header(); ?>
     data-1800="top: -5em; opacity: 0;"
 >
     <p>
-      I have been coding since I was 14 in my first high school programming class, and I’ve become a major polyglot programmer since then.
+      My love of coding started when I was 14 in my first high school programming class, and I’ve become a major polyglot programmer since then.
       Starting with <span class='hi'>Visual Basic</span> (anyone remember that?), I then went on to learn <span class="hi">Java</span>, <span class="hi">C</span>,
       <span class="hi">C++</span>, <span class="hi">HTML</span>, <span class="hi">CSS</span>, <span class="hi">VB.NET</span>, <span class="hi">C#</span>,
       <span class="hi"> Javascript</span>, <span class="hi">SQL</span>, <span class="hi">NoSQL DBs</span>, <span class="hi">Ruby</span>,
       <span class="hi">Python</span>, <span class="hi">PHP</span>, <span class="hi">Shell</span>, <span class="hi">Git</span>, <span class="hi">Io</span>,
-      <span class="hi">Prolog</span>…. The list could go on and on.
+      <span class="hi">Prolog</span>…. The list goes  on and on.
     </p>
   </div>
   <div class="content-2"
@@ -97,10 +98,10 @@ get_header(); ?>
     data-2100="top: 4em; opacity: 1;"
     data-2400="top: -5em; opacity: 0;"
 >
-    <p>My background is that of a back-end developer, specializing in object-oriented programming in both software and web development.
+    <p>I typically play the role of  a back-end developer, working with multiple programming paradigms in software, web, and mobile development.
       I prefer to program in a Unix-based environment, utilizing the terminal heavily for everything.
-      My main skills here are <span class="hi">Ruby/Rails</span>, <span class="hi">PHP</span>, <span class="hi">Python</span>, <span class="hi">SQL</span>,
-      <span class="hi">NoSQL</span>, <span class="hi">APIs</span>, <span class="hi">Node.js</span>, <span class="hi">Angular.js</span>, <span class="hi">Ember.js</span>, and a little bit of <span class="hi">Go</span>.</p>
+      My main skills here are <span class="hi">Ruby/Rails</span>, <span class="hi">PHP/Laravel</span>, <span class="hi">Python</span>, <span class="hi">C#</span>, <span class="hi">SQL</span>,
+      <span class="hi">NoSQL</span>, <span class="hi">APIs</span>, <span class="hi">Node.js</span>, <span class="hi">Git</span>, <span class="hi">Shell</span>, <span class="hi">Vim</span>, and <span class="hi">Tmux</span>.
   </div>
   <div class="content-4"
     data-2400="top: -12em; opacity: 0;"
@@ -108,19 +109,19 @@ get_header(); ?>
     data-2700="top: -8em; opacity: 1;"
     data-2900="top: -14em; opacity: 0;"
 >
-    <p>I don't work only on the back-end though; I've used a bunch of <span class="hi">HTML5</span>, <span class="hi">Sass</span>,
-      <span class="hi">Javascript</span>, <span class="hi">build automation</span>, CMSs like <span class="hi">Wordpress</span>,
-      <span class="hi">ModX</span>, <span class="hi">Umbraco</span>, and <span class="hi">Craft CMS</span>, and some enterprise web app development using <span class="hi">.NET</span>, <span class="hi">Rails</span>, <span class="hi">Angular</span>, and <span class="hi">React</span>. I use <span class="hi">Foundation</span> and
-      <span class="hi">Bootstrap</span> when I need to, but I usually opt to build completely custom styles.
-      Check out my projects to see what I’ve been working on!</p>
+    <p>That said, I don't work only on the back-end though; I've used a bunch of <span class="hi">HTML5</span>, <span class="hi">Sass</span>,
+      and all the major <span class="hi">JavaScript</span> frameworks (<span class="hi">Vue</span>, <span class="hi">React</span>, <span class="hi">Angular</span>, and even <span class="hi">Ember</span>). I've also built plenty of projects using CMSs like <span class="hi">Wordpress</span>,
+      <span class="hi">Craft</span>, <span class="hi">Umbraco</span>, and <span class="hi">ModX</span>. For styling, I use
+      <span class="hi">Bootstrap</span> when it makes sense, but I really prefer to build completely custom styles.
+      Check out my <a style="color: white;" href="<?php echo get_permalink(30); ?>" target="_blank">projects</a> to see what I’ve been working on!</p>
   </div>
 </div>
 </div>
 
 </div>
-<div id="slide4" class="slide"  style="background-image: url('/assets/images/dist/ak-superman.jpg');"
-  data-bottom-top="background-position: 50% 70%;"
-  data-top-bottom="background-position: 50% 30%;"
+<div id="slide4" class="slide"  style="background-image: url('/assets/images/dist/Krauss-Carsyn-Abrams-Photography2-465.jpg');"
+  data-bottom-top="background-position: 50% 50%;"
+  data-top-bottom="background-position: 50% 10%;"
 >
     <div class="curtain"
     data-2300="height: 100%; opacity: .7;"
@@ -142,17 +143,20 @@ get_header(); ?>
     data-3400="opacity: 0;top: -8em; left: -8em;"
     data-3600="opacity: 1;top: 0em; left: 0em;"
 >
-        <p>I love <span class="hi">tea</span>. Not your everyday grocery store tea though.
-          I usually purchase my tea from special vendors with direct tea farmer relationships so that I can know exactly what province it was plucked in,
-          and what month it was plucked.</p>
+        <p>I love <span class="hi">tea</span>, and that's putting it lightly.
+I've been a tea enthusiast for over a decade now. My favorites
+are Japanese greens and Chinese oolongs, but I like to brew anything as long as it's good quality and got some history behind it.
+          </p>
       </div>
       <div class="interest-text-wrapper small-right"
     data-3700="opacity: 0;top: -8em; left: 8em;"
     data-3900="opacity: 1;top: 0em; left: 0em;"
 >
-        <p>I love <span class="hi">walking my dog.</span> I find it super relaxing, I get to experience raw sunlight (something us developers don’t do often enough) and I’m giving both me and my dog exercise.</p>
+        <p>I love <span class="hi">my dogs</span>.
+We've got a dutch shepherd and a samoyed, and they're a big part of what makes our house a home.
+No matter how old they get, they always play like puppies together.</p>
       </div>
-      <img class="left" src="/assets/images/dist/jackson.jpg" alt=""
+      <img class="left" src="/assets/images/dist/luna.jpg" alt=""
         data-3800="opacity: 0;top: -8em; left: -8em;transform: rotate(0deg);"
         data-4000="opacity: 1;top: 0em; left: 0em;transform: rotate(-10deg);"
       />
@@ -160,9 +164,9 @@ get_header(); ?>
 </div>
 
 </div>
-<div id="slide6" class="slide"  style="background-image: url('/assets/images/dist/princess-layla-and-me.jpg');"
-  data-bottom-top="background-position: 50% 70%;"
-  data-top-bottom="background-position: 50% 30%;"
+<div id="slide6" class="slide"  style="background-image: url('/assets/images/dist/Krauss-Carsyn-Abrams-Photography2-92.jpg');"
+  data-bottom-top="background-position: 50% 50%;"
+  data-top-bottom="background-position: 50% 10%;"
 >
 
 <div class='content'>
@@ -171,27 +175,33 @@ get_header(); ?>
     data-4200="opacity: 0;top: -8em; left: -8em;"
     data-4400="opacity: 1;top: 0em; left: 0em;"
 >
-        <p>I love <span class='hi'>learning</span>. Anything from educational podcasts, YouTube videos, books - if I can learn from it, then I'm interested.
-        I know most people like learning things - but this has become a serious passion of mine. I truly want to learn everything there is to learn.</p>
+        <p>I love <span class='hi'>longboarding</span>. I got into it in college as
+a quick way to get between classes, and I just fell in love with it. I don't do any fancy tricks,
+I just enjoy cruising and carving down hills, and getting rid of any stress from the day. It's a fun way to walk my dogs too!
+        </p>
       </div>
       <div class="interest-text-wrapper"
     data-4400="opacity: 0;top: -8em; left: 8em;"
     data-4600="opacity: 1;top: 0em; left: 0em;"
 >
-        <p>I love <span class="hi">craft beer</span>. I love experiencing all the various styles of beer; as long as it's not a session beer, then I'm probably gonna like it. I even homebrewed some batches of beer for a while!
-          I was born in Germany, so maybe it stems from that a little bit. You’ll never see me go to a bar and order a bottle.
-          It has to be on tap. My friends call me a beer geek, but I just don’t want to appreciate it unless it’s in its finest form.</p>
+        <p>I love <span class="hi">craft beer</span> and <span class="hi">making cocktails</span>.
+If you've seen <a href="<?php echo get_permalink(836); ?>" target="_blank">My History with Beer</a>,
+then you know beer is a part of my heritage. I was born in Germany, after all.
+After I turned 21, it rocketed into a full-blown passion.
+As far as cocktails go, I've got a full at-home cocktail setup to craft whatever you can dream.
+It may take me 10 minutes to make a drink, but you bet it'll be made right!
+          </p>
       </div>
       <div class="interest-text-wrapper small-left"
     data-4600="opacity: 0;top: -8em; left: -8em;"
     data-4800="opacity: 1;top: 0em; left: 0em;"
 >
-        <p>I love <span class="hi">Layla</span>. We have been dating for over 12 years, and recently got married!
-          She's the coolest, most down-to-earth person I've ever met, and it's so awesome to be the lucky guy who gets to have her.
-          I couldn’t imagine a life without her, and hanging out with her – doing anything – is always my favorite thing to do. I love her.
+        <p>I love <span class="hi">Layla</span>. We've been together since 2006! She's my partner in crime when
+          it comes to travelling the globe, playing tennis, bingewatching TV series, and pretty much everything in between.
+          She's the sweetest, most thoughtful and honest person I know. I can't imagine a life without her. I love her.
           She’s an incredibly talented dietitian too!</p>
       </div>
-      <img class="right" src="/assets/images/dist/layla-and-me.jpg" alt=""
+      <img class="right" src="/assets/images/dist/Aaron-Layla-Carsyn-Abrams-Photography-35.jpg" alt=""
         data-4700="opacity: 0;top: -8em; left: 8em;transform: rotate(0deg);"
         data-4900="opacity: 1;top: 0em; left: 0em;transform: rotate(10deg);"
       />
